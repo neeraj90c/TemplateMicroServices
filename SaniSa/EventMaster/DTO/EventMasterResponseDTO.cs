ecf4fd3d0669523995bc3d3c3d989e399905bc33
Sumeet Goliwar <sumeetgoliwar100@gmail.com>
2024-09-22 20:25:20 +0530

    Added Event Id

--- SaniSa/EventMaster/DTO/EventMasterResponseDTO.cs
+++ SaniSa/EventMaster/DTO/EventMasterResponseDTO.cs
@@ -2,6 +2,7 @@
 {
     public class EventMasterResponseDTO
     {
+        public int EventId { get; set; }
         public string? EventName { get; set; }
         public string? EventCode { get; set; }
         public string? EventDesc { get; set; }
